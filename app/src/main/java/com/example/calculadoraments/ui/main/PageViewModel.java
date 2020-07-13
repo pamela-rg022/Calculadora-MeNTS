74cc55d089db3d7f46ae8c389ca0e4806a65ff44
pamela-rg022 <p.ramirezg@uniandes.edu.co>
2020-07-13 15:39:43 -0500

    AHORA SIII! :) últimos cambios de textos y espacios

--- app/src/main/java/com/example/calculadoraments/ui/main/PageViewModel.java
+++ app/src/main/java/com/example/calculadoraments/ui/main/PageViewModel.java
@@ -18,10 +18,10 @@ public class PageViewModel extends ViewModel {
                     return " ● MeNTS es una calculadora que permite a los profesionales médicos dar prioridad a aquellas intervenciones quirúrgicas que se vieron afectadas por la aparición del nuevo COVID-19." +
                             "\n" +
                             "\n" +
-                            " ● MeNtS apoya a la toma de decisiones basada en una calificación objetiva que permite decidir si realizar o no un procedimiento quirúrgico en tiempos de pandemia." +
+                            " ● MeNTS apoya a la toma de decisiones basada en una calificación objetiva que permite decidir si realizar o no un procedimiento quirúrgico en tiempos de pandemia." +
                             "\n" +
                             "\n" +
-                            "● Los puntajes de MeNTS de umbral superior e inferior se pueden asignar y ajustar dinámicamente para responder a la disponibilidad inmediata y anticipada de recursos y condiciones locales al tiempo que se preserva la capacidad de la sala de operaciones para casos de trauma, emergencia y casos altamente urgentes.";
+                            "● Los puntajes de MeNTS de umbral superior e inferior se pueden asignar y ajustar dinámicamente para responder a la disponibilidad inmediata y anticipada de recursos y condiciones locales al tiempo que se preserva la capacidad de la sala de operaciones para casos de trauma, emergencia y casos altamente urgentes. Por esta razón Calculadora MeNTS provee únicamente el resultado acumulado mas no una interpretación del mismo.";
                 case 2:
                     return " ● Diseñada en una sencilla escala de 1 a 5 donde un valor más alto corresponde a un peor resultado perioperatorio del paciente, un mayor riesgo de transmisión de COVID-19 al equipo de atención y/o un mayor uso de recursos hospitalarios durante la pandemia. " +
                             "\n" +
