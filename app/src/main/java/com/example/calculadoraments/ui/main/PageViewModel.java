685473f25133be7570dac3c1d4836915f9528122
pamela-rg022 <p.ramirezg@uniandes.edu.co>
2020-07-14 10:30:17 -0500

    Quito espacios bullet points info tabs

--- app/src/main/java/com/example/calculadoraments/ui/main/PageViewModel.java
+++ app/src/main/java/com/example/calculadoraments/ui/main/PageViewModel.java
@@ -15,24 +15,24 @@ public class PageViewModel extends ViewModel {
             switch (input)
             {
                 case 1:
-                    return " ● MeNTS es una calculadora que permite a los profesionales médicos dar prioridad a aquellas intervenciones quirúrgicas que se vieron afectadas por la aparición del nuevo COVID-19." +
+                    return "● MeNTS es una calculadora que permite a los profesionales médicos dar prioridad a aquellas intervenciones quirúrgicas que se vieron afectadas por la aparición del nuevo COVID-19." +
                             "\n" +
                             "\n" +
-                            " ● MeNTS apoya a la toma de decisiones basada en una calificación objetiva que permite decidir si realizar o no un procedimiento quirúrgico en tiempos de pandemia." +
+                            "● MeNTS apoya a la toma de decisiones basada en una calificación objetiva que permite decidir si realizar o no un procedimiento quirúrgico en tiempos de pandemia." +
                             "\n" +
                             "\n" +
                             "● Los puntajes de MeNTS de umbral superior e inferior se pueden asignar y ajustar dinámicamente para responder a la disponibilidad inmediata y anticipada de recursos y condiciones locales al tiempo que se preserva la capacidad de la sala de operaciones para casos de trauma, emergencia y casos altamente urgentes. Por esta razón Calculadora MeNTS provee únicamente el resultado acumulado mas no una interpretación del mismo.";
                 case 2:
-                    return " ● Diseñada en una sencilla escala de 1 a 5 donde un valor más alto corresponde a un peor resultado perioperatorio del paciente, un mayor riesgo de transmisión de COVID-19 al equipo de atención y/o un mayor uso de recursos hospitalarios durante la pandemia. " +
+                    return "● Diseñada en una sencilla escala de 1 a 5 donde un valor más alto corresponde a un peor resultado perioperatorio del paciente, un mayor riesgo de transmisión de COVID-19 al equipo de atención y/o un mayor uso de recursos hospitalarios durante la pandemia. " +
                             "\n" +
                             "\n" +
-                            " ● La escala se aplica a distintos aspectos dentro de tres factores generales: procedimiento, paciente y enfermedad." +
+                            "● La escala se aplica a distintos aspectos dentro de tres factores generales: procedimiento, paciente y enfermedad." +
                             "\n" +
                             "\n"+
-                            " ● La calculadora se basa en medidas objetivas y probabilidades clínicas percibidas que traducidas en la escala generada y acumulando los diferentes resultados se obtiene un puntaje acumulado MeNTS que permite apoyar la toma de decisiones." +
+                            "● La calculadora se basa en medidas objetivas y probabilidades clínicas percibidas que traducidas en la escala generada y acumulando los diferentes resultados se obtiene un puntaje acumulado MeNTS que permite apoyar la toma de decisiones." +
                             "\n" +
                             "\n" +
-                            " ● El puntaje acumulativo MeNTS puede variar entre 21 y 105, donde un valor más alto implica un procedimiento menos justificado y con uno más bajo se puede tener una mayor consideración para realizar el procedimiento." ;
+                            "● El puntaje acumulativo MeNTS puede variar entre 21 y 105, donde un valor más alto implica un procedimiento menos justificado y con uno más bajo se puede tener una mayor consideración para realizar el procedimiento." ;
             }
             return null;
             //return "Hello world from section: " + input;
